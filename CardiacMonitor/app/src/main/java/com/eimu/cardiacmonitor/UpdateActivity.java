d140a638d809a1026e21049a6e9f51314a85806b
Tasfia-faija <tasfia719@gmail.com>
2022-07-13 19:06:48 +0600

    update xml file added by faija

---
+++ CardiacMonitor/app/src/main/java/com/eimu/cardiacmonitor/UpdateActivity.java
@@ -0,0 +1,17 @@
+package com.eimu.cardiacmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    EditText date_input
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
